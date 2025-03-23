801a3b10c0231d890193a3a917b3829e1c424a74
standbyplease <2023.jay.kerkar@ves.ac.in>
2025-03-23 06:28:42 +0000

    Update README and layout to include Prisma integration and navigation links

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,56 @@
-<script lang="ts">
-	import '../app.css';
-	let { children } = $props();
-</script>
+<nav>
+	<div class="container">
+		<a href="/" class="logo">SvelteKit + tRPC</a>
+		<ul>
+			<li><a href="/">Home</a></li>
+			<li><a href="/users">Users</a></li>
+		</ul>
+	</div>
+</nav>
 
-{@render children()}
+<slot />
+
+<style>
+	nav {
+		background: #333;
+		color: white;
+		padding: 1rem 0;
+		margin-bottom: 2rem;
+	}
+
+	.container {
+		max-width: 1200px;
+		margin: 0 auto;
+		padding: 0 1rem;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+	}
+
+	.logo {
+		font-size: 1.25rem;
+		font-weight: bold;
+		color: white;
+		text-decoration: none;
+	}
+
+	ul {
+		display: flex;
+		list-style: none;
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		margin-left: 1.5rem;
+	}
+
+	a {
+		color: white;
+		text-decoration: none;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+</style>
